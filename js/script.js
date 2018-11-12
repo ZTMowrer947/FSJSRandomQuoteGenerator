637c9a7866975012d3b51c364d00483bcdeda818
ZTMowrer947 <Zackmowrer@gmail.com>
2018-11-11 20:10:13 -0500

    Add printQuote function
    Remove treehouse comments

--- js/script.js
+++ js/script.js
@@ -1,15 +1,7 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 1 - A Random Quote Generator
-******************************************/
+// Treehouse FSJS Techdegree Unit 1 Project
+// Random Quote Generator
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/*** 
-Recommended: (I plan to do this but I am finding difficulty in finding either of these at the moment) 
-- Add at least one `year` and/or `citation` property to at least one quote object.
-***/
+// TODO: Add at least one `year` and/or `citation` property to at least one quote object.
 
 // Define array of quotes
 let quotes = [
@@ -78,23 +70,26 @@ function getRandomQuote() {
     return quotes[index];
 }
 
-/***
- Create the `printQuote` function to: 
-- call the `getRandomQuote` function and assign it to a variable.
-- use the properties of the quote object stored in the variable to 
-    create your HTML string.
-- use conditionals to make sure the optional properties exist before 
-    they are added to the HTML string.
-- set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
+// Quote printing function
+function printQuote() {
+    // Get random quote
+    let quote = getRandomQuote();
 
+    // Concatenate quote into HTML string
+    let quoteHtml = "<p class=\"quote\">" + quote.quote + "</p>";
 
+    // Append source to HTML string
+    quoteHtml += "<p class=\"source\">" + quote.source;
 
-/***
- When the "Show another quote" button is clicked, the event listener 
-below will be triggered, and it will call, or "invoke", the `printQuote` 
-function. So do not make any changes to the line of code below this 
-comment.
-***/
+    // Close HTML element and string
+    quoteHtml += "</p>";
 
+    // Get quote-box div
+    let quoteBox = document.getElementById("quote-box");
+
+    // Set innerHTML to HTML string
+    quoteBox.innerHTML = quoteHtml;
+}
+
+// Call printQuote function when loadQuote button is clicked
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
